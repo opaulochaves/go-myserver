85a0c1d676d1085db5bae27876ac24486d28b3d5
Paulo Chaves <opaulochaves@gmail.com>
2022-12-01 13:04:33 -0300

    create users and notes table

---
+++ db/migrations/000001_init.up.sql
@@ -0,0 +1,24 @@
+SET TIMEZONE="UTC";
+
+CREATE TABLE IF NOT EXISTS users(
+  id serial PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  updated_at TIMESTAMP NULL
+);
+
+CREATE TABLE IF NOT EXISTS notes(
+  id serial PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  content VARCHAR NOT NULL,
+  user_id INTEGER NOT NULL,
+  attrs JSONB NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  updated_at TIMESTAMP NULL,
+  CONSTRAINT fk_users
+    FOREIGN KEY(user_id)
+    REFERENCES users(id)
+);
